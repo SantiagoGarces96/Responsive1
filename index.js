ffb0dbb7e76c8965e2f88630a042c9ac0a4756e4
SantiagoGarces96 <santiagogarcesstory@gmail.com>
2023-02-16 16:19:09 -0500

    ya se pudo

---
+++ index.js
@@ -0,0 +1,15 @@
+const hamburger=document.querySelector('#hamburguer')
+const close=document.querySelector('#close')
+const menu=document.querySelector('#menu')
+
+hamburger.addEventListener('click',()=>{
+    hamburger.style.display="none";
+    menu.style.display="block";
+    close.style.display="block";
+})
+
+close.addEventListener('click',()=>{
+    hamburger.style.display="block";
+    menu.style.display="none";
+    close.style.display="none";
+})
